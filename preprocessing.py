d684e45a3d00a7c2d27afa048dcd145b9916fc8e
hurditya <44564601+hurditya@users.noreply.github.com>
2019-10-12 18:07:02 +0530

    Add files via upload

---
+++ preprocessing.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+dataset = pd.read_csv(' .csv')                  #Enter dataset name here
+
+dataset = pd.DataFrame(dataset)
+
+data = dataset[dataset['Kind'].str.contains('Class') == True]
+
+data = data[['Kind', 'Name', 'CountClassBase', 'CountClassCoupled', 'CountClassDerived', 
+              'CountDeclClassMethod', 'CountDeclClassVariable', 
+              'CountDeclInstanceMethod', 'CountDeclInstanceVariable', 
+              'CountDeclMethod', 'CountDeclMethodAll', 'CountDeclMethodDefault',
+              'CountDeclMethodPrivate', 'CountDeclMethodProtected', 'CountDeclMethodPublic', 
+              'MaxInheritanceTree', 'PercentLackOfCohesion']]
+
+data.to_csv(' .csv')                             #Enter destination file here
