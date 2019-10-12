a56ecf2d0a03c82e522fea080c2450bfb30e5afa
hurditya <44564601+hurditya@users.noreply.github.com>
2019-10-12 18:25:10 +0530

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -6,11 +6,11 @@ dataset = pd.DataFrame(dataset)
 
 data = dataset[dataset['Kind'].str.contains('Class') == True]
 
-data = data[['Kind', 'Name', 'CountClassBase', 'CountClassCoupled', 'CountClassDerived', 
-              'CountDeclClassMethod', 'CountDeclClassVariable', 
-              'CountDeclInstanceMethod', 'CountDeclInstanceVariable', 
-              'CountDeclMethod', 'CountDeclMethodAll', 'CountDeclMethodDefault',
-              'CountDeclMethodPrivate', 'CountDeclMethodProtected', 'CountDeclMethodPublic', 
-              'MaxInheritanceTree', 'PercentLackOfCohesion']]
+data = data[['Kind', 'Name', 'CountLine' ,'CountLineCode', 'CountLineBlank', 'CountLineComment', 
+             'CountClassBase', 'CountClassCoupled', 'CountClassDerived', 'CountDeclClassMethod', 
+             'CountDeclClassVariable', 'CountDeclInstanceMethod', 'CountDeclInstanceVariable', 
+             'CountDeclMethod', 'CountDeclMethodAll', 'CountDeclMethodDefault',
+             'CountDeclMethodPrivate', 'CountDeclMethodProtected', 'CountDeclMethodPublic', 
+             'MaxInheritanceTree', 'PercentLackOfCohesion']]
 
 data.to_csv(' .csv')                             #Enter destination file here
